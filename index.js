eb82f9cdb0f447254efd5c14385e6639980033f3
codewithshamim <shamimislamonline@gmail.com>
2022-12-03 18:40:19 +0600

    socket.io initial setup added

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+
 const colors = require("colors");
 const database = require("./utils/database");
 const userRoute = require("./routes/v1/user.route");
@@ -40,9 +48,19 @@ app.use("/api/v1/lap", lapRoute);
 app.use("/api/v1/posts", postRoute);
 app.use("/api/v1/comments", commentRoute);
 
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on(("message"), msg=>{
+        console.log(msg);
+    })
+});
+
 // server
-app.listen(port, () => {
-    console.log(`App is running on port ${port}`.yellow.bold);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`.yellow.bold);
 });
 
 // globaly error handler 
