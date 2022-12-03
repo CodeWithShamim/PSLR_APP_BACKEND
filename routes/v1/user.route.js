1fc097e880e948a2ecf1ac4cc197c785444a7958
codewithshamim <shamimislamonline@gmail.com>
2022-12-03 20:20:52 +0600

    update

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../../controllers/user.controller");
+const { authorization } = require("../../middleware/authorization");
 const verifyToken = require("../../middleware/verifyToken");
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.post("/login", userController.login);
 router.post("/uploadImage", verifyToken, userController.uploadImage);
 router.post("/updateProfile", verifyToken, userController.updateProfile);
 
-router.get("/getMe", verifyToken, userController.getMe);
+router.get("/getMe", verifyToken, authorization("user"), userController.getMe);
 
 router.post("/forgotPassword", userController.forgotPassword);
 router.post("/updatePassword", userController.updatePassword);
--- utils/emailSender.js
+++ utils/emailSender.js
@@ -6,6 +6,7 @@ const oAuth2Client = new google.auth.OAuth2(
     process.env.CLIENT_SECRET,
     "https://developers.google.com/oauthplayground"
 );
+
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 module.exports.sendMailWithGmail = async (data) => {
