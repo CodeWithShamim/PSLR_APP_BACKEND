abf91ac9c1663eaf5c0857d111af5f66f545f6e5
codewithshamim <shamimislamonline@gmail.com>
2022-12-10 19:00:46 +0600

    socket config added

--- socket.js
+++ socket.js
@@ -1,12 +1,42 @@
 const socket = (io) => {
+    let users = [];
+
+    const addUser = (userId, socketId) => {
+        const isExits = users.some((user) => user.userId === userId);
+        if (!isExits) users.push({ userId, socketId })
+    }
+    const getUser = (userId) => {
+        return users.find((user) => user.userId === userId)
+    }
+    const removeUser = (socketId) => {
+        users = users.filter((user) => user.socketId !== socketId);
+    }
+
     io.on('connection', (socket) => {
-        console.log('User connected');
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
-        socket.on(("message"), msg => {
-            io.emit("message", msg)
+        console.log("user connected...");
+
+        // add user 
+        socket.on(("addUser"), userId => {
+            addUser(userId, socket.id)
+            io.emit("getUsers", users)
         })
+
+        // send message 
+        socket.on("sendMessage", data => {
+            let user;
+
+            user = getUser(data.senderId)
+            io.to(user?.socketId).emit("getMessage", data)
+            user = getUser(data.receiverId)
+            if (user?.socketId) io.to(user.socketId).emit("getMessage", data)
+        })
+
+        // remove user 
+        socket.on('disconnect', () => {
+            console.log("user Disconnected...");
+            removeUser(socket.id)
+            io.emit("getUsers", users)
+        });
     });
 }
 
